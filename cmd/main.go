c19e006261de0dba93f9d0b78870a39910b1681c
Лабанова Рита <labanovams@gmail.com>
2025-12-14 21:45:25 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.Ldate)
+
+	srv := server.NewServer(logger)
+
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,45 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func isMorse(input string) bool {
+	for _, char := range input {
+		if char != '.' && char != '-' && char != ' ' {
+			return false
+		}
+	}
+	return true
+}
+
+func Convert(input string) (string, error) {
+	if input == "" {
+		return "", fmt.Errorf("Empty line")
+	}
+
+	tabInput := strings.TrimSpace(input)
+	if tabInput == "" {
+		return "", fmt.Errorf("Only tab")
+	}
+
+	if isMorse(tabInput) {
+		res := morse.ToText(tabInput)
+
+		if res == "" {
+			return "", fmt.Errorf("Error convert")
+		}
+
+		return res, nil
+	}
+
+	res := morse.ToMorse(tabInput)
+	if res == "" {
+		return "", fmt.Errorf("Error convert")
+	}
+
+	return res, nil
+}
